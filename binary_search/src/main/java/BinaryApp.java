4c5a9664409a0f946388ccc4ef1489bea30bb4c2
SergeyAnosov <Anosov-sa@yandex.ru>
2023-11-12 12:09:56 +0300

    from note

--- binary_search/src/main/java/BinaryApp.java
+++ binary_search/src/main/java/BinaryApp.java
@@ -3,7 +3,7 @@ public class BinaryApp {
     public static void main(String[] args) {
         BinarySearch bs = new BinarySearch();
         int[] ar1 = {1, 3, 4, 6, 8, 10};
-        System.out.println(bs.binarySearch(ar1, -5));
+        System.out.println(bs.binarySearch(ar1, 6));
 
 
     }
